57cd4c1b69b171184f498c1c7d528e04622163e6
Pavel Podviaznikov <pavel.podviaznikov@gmail.com>
2015-01-29 22:33:49 +0200

    added button handler

---
+++ app.js
@@ -0,0 +1,12 @@
+$(document).on('ready', function(){
+	getAnswer();
+});
+
+function getAnswer(){
+	$('#send-answer').click(function(){
+		var answerValue = $('#answer-field').val();
+		$('#messages-window').append('<div class="avatar">'+'<img src="ars.jpg"/>'+'</div>');
+		$('#messages-window').append('<div class="message">'+answerValue+'</div>');
+		console.log(answerValue);
+	});
+}
