705ca4e9c4c966191a2edf22f0eec93eb0618204
Denis Bolkovenko <bolkoven@ukr.net>
2015-01-30 15:58:54 +0200

    empty string checker add

--- public/main.js
+++ public/main.js
@@ -5,15 +5,19 @@ $(document).on('ready', function(){
 function submitAnswer(){
 	$('#submit-answer').click(function(){
 		var answerValue = $('#user-answer').val();
-		$('#game-content').append('<div class="col-lg-2 col-md-2 col-sm-2 avatar">' +'<img src="ars.jpg"/>'+ '</div>');
-		$('#game-content').append('<div class="col-lg-10 col-md-10 col-sm-10 message"><p>' + answerValue + '</p></div>');
+        if(answerValue !== ''){
+            $('#game-content').append('<div class="col-lg-2 col-md-2 col-sm-2 avatar">' +'<img src="ars.jpg"/>'+ '</div>');
+            $('#game-content').append('<div class="col-lg-10 col-md-10 col-sm-10 message"><p>' + answerValue + '</p></div>');
+        }
 	});
 
     $('#user-answer').keydown(function(e){
         var answerValue = $('#user-answer').val();
         if(e.keyCode === 13){
-            $('#game-content').append('<div class="col-lg-2 col-md-2 col-sm-2 avatar">' +'<img src="ars.jpg"/>'+ '</div>');
-            $('#game-content').append('<div class="col-lg-10 col-md-10 col-sm-10 message"><p>' + answerValue + '</p></div>');
+            if(answerValue !== ''){
+                $('#game-content').append('<div class="col-lg-2 col-md-2 col-sm-2 avatar">' +'<img src="ars.jpg"/>'+ '</div>');
+                $('#game-content').append('<div class="col-lg-10 col-md-10 col-sm-10 message"><p>' + answerValue + '</p></div>');
+            }
         }
     });
 }
