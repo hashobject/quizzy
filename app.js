73b7b20e87e01bcb92ce59bc4e21442ed1c2b439
Pavel Podviaznikov <pavel.podviaznikov@gmail.com>
2015-01-31 11:39:12 +0200

    Q&A loging in node console

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 'use strict';
-var express = require('express')
-var app = express();
-var fs = require('fs');
-var jsonData;
+var express = require('express'),
+	fs = require('fs'),
+	JSON5 = require('json5'),
+	app = express(),
+	jsonData;
+
+var capitals = [],
+	countries = [];
 
 fs.readFile('./public/freebase_countries.json', 'utf8', function(err, data){
-    jsonData = data;
-    console.log(jsonData);
+    jsonData = JSON5.parse(data);
+    getDataFromJson(jsonData, capitals, countries);
+   
+   for(var i = 0; i<10; i++) {
+    	console.log("Question " + (i+1) + ":");
+    	console.log(countries[i]);
+	    console.log("Answer " + (i+1) + ":");
+	    console.log(capitals[i]);
+	    console.log('');
+    }
 });
-
 
 app.use(express.static('public'));
 
@@ -17,6 +28,20 @@ var server = app.listen(3000, function () {
   var host = server.address().address
   var port = server.address().port
 
-  console.log('Example app listening at http://%s:%s', host, port)
+  console.log('Example app listening at http://%s:%s', host, port);
 
 });
+
+function getDataFromJson(json, cpt, cnt) {
+	for(var i in json){
+		if(json[i].capital !== null) {
+			cpt.push(json[i].capital);
+			cnt.push(json[i].name);
+		}
+		else {
+			cpt.push(json[i].name);
+			cnt.push(json[i].name);
+		}
+	}
+}
+
