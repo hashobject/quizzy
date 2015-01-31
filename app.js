729d3dde9aeb14bed65dd1b7eef1454105129338
Pavel Podviaznikov <pavel.podviaznikov@gmail.com>
2015-02-01 00:10:21 +0200

    correct endings on test data.  not full ckecking (dont check all countries endings, just few)

--- app.js
+++ app.js
@@ -7,10 +7,16 @@ var express = require('express'),
 
 var questions = [];
 
+var originCountries = ['Україна', 'Англія', 'Франція', 'Іспанія', 'Німеччина', 'Чехія', 'Молдова', 'Уельс', 'Китай', 'Азербайджан'];
+
 fs.readFile('./public/freebase_countries.json', 'utf8', function(err, data){
     jsonData = JSON5.parse(data);
     getDataFromJson(jsonData, questions);
-    console.log(questions);
+    var correctNameEnding;
+    for(var i in originCountries) {
+        correctNameEnding = changeCountryNameEnding(originCountries[i]);
+        console.log(correctNameEnding);
+    }
 });
 
 app.use(express.static('public'));
@@ -33,5 +39,41 @@ function getDataFromJson(json, question) {
 			question.push({"name": json[i].name, "capital": json[i].name});
 		}
 	}
+}
+
+function changeCountryNameEnding(country) {
+    var countryLength = country.length,
+        endings = country[countryLength-1],
+        nameWithoutEnding = country.slice(0, countryLength-1),
+        correctName;
+
+    switch(endings) {
+        case 'а':
+            correctName = nameWithoutEnding+'и';
+            break;
+        case 'я':
+            correctName = nameWithoutEnding+'ї';
+            break;
+        case 'й':
+            correctName = nameWithoutEnding+'я';
+            break;
+        case 'н':
+            correctName = nameWithoutEnding+'на';
+            break;
+        case 'р':
+            correctName = nameWithoutEnding+'ра';
+            break;
+        case 'с':
+            correctName = nameWithoutEnding+'са';
+            break;
+        case 'ь':
+            correctName = nameWithoutEnding+'і';
+            break;
+        case 'м':
+            correctName = nameWithoutEnding+'ма';
+            break;
+    }
+
+    return correctName;
 }
 
