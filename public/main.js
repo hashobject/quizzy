0d3215788fea166658ecd88c4e5dcb5708529f54
Pavel Podviaznikov <pavel.podviaznikov@gmail.com>
2015-02-02 22:44:13 +0200

    new commit

--- public/main.js
+++ public/main.js
@@ -13,6 +13,7 @@ function submitAnswer(){
             $.post('/messages', {message: answerValue});
             displayAnswer();
             $('#user-answer').val('');
+            $('#game-content').scrollTop(1E10);
         }
 	});
 
@@ -24,6 +25,7 @@ function submitAnswer(){
                 $.post('/messages', {message: answerValue});
                 displayAnswer();
                 $('#user-answer').val('');
+                $('#game-content').scrollTop(1E10);
             }
         }
     });
@@ -36,6 +38,7 @@ function displayAnswer() {
         if(messages.length !== 0) {
             $('#game-content').children('.row').remove();
             for(var i in messages) {
+
                 var myHtml =  '<div class="row">'+
                     '<div class="col-lg-2 col-md-2 col-sm-2 avatar"><img src="ars.jpg"/></div>'+
                     '<div class="col-lg-10 col-md-10 col-sm-10 message"><p>' + messages[i] + '</p></div>'+
@@ -43,7 +46,6 @@ function displayAnswer() {
 
                 $('#game-content').append(myHtml);
             }
-            $('#game-content').scrollTop(1E10);
         }
     });
 
