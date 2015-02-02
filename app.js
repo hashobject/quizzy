1698f6a7d81dc64d9146d3a4b64bb174385e786a
PavelPodviaznikov <pavel.podviaznikov@gmail.com>
2015-02-02 13:50:31 +0200

    new commit. chenge app.js

--- app.js
+++ app.js
@@ -1,17 +1,6 @@
 'use strict';
 var express = require('express'),
-	fs = require('fs'),
-	JSON5 = require('json5'),
-	app = express(),
-	jsonData;
-
-var questions = [];
-
-fs.readFile('./public/freebase_countries.json', 'utf8', function(err, data){
-    jsonData = JSON5.parse(data);
-    getDataFromJson(jsonData, questions);
-    console.log(questions);
-});
+	app = express();
 
 app.use(express.static('public'));
 
@@ -24,14 +13,4 @@ var server = app.listen(3000, function () {
 
 });
 
-function getDataFromJson(json, question) {
-	for(var i in json){
-		if(json[i].capital !== null) {
-			question.push({"name": json[i].name, "capital": json[i].capital});
-		}
-		else {
-			question.push({"name": json[i].name, "capital": json[i].name});
-		}
-	}
-}
 
