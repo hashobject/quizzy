68029f00da1f1cb1a3a1e4108f580efbb70b5a3c
Pavel Podviaznikov <pavel.podviaznikov@gmail.com>
2015-02-03 23:41:44 +0200

    added nickname

--- public/main.js
+++ public/main.js
@@ -1,38 +1,42 @@
 $(document).on('ready', function(){
 	submitAnswer();
-    displayMessage();
+    createNewMessage();
 });
 
 function submitAnswer(){
 	$('#submit-answer').click(function(){
 		var answerValue = $('#user-answer').val();
-        if(answerValue !== ''){
+        var user = $('#usernick').val();
+        if(answerValue !== '' && user !== ''){
             var socket = io();
-            socket.emit('message', answerValue);
+            socket.emit('message', {user: user, message: answerValue});
             $('#user-answer').val('');
+            $('#usernick').val('');
             $('#game-content').scrollTop(1E10);
         }
 	});
 
     $('#user-answer').keydown(function(e){
         var answerValue = $('#user-answer').val();
+        var user = $('#usernick').val();
         if(e.keyCode === 13){
-            if(answerValue !== ''){
+            if(answerValue !== '' && user !== ''){
                 var socket = io();
-                socket.emit('message', answerValue);
+                socket.emit('message', {user: user, message: answerValue});
                 $('#user-answer').val('');
+                $('#usernick').val('');
                 $('#game-content').scrollTop(1E10);
             }
         }
     });
 }
 
-function displayMessage() {
+function createNewMessage() {
     var socket = io();
     socket.on('message', function(msg){
         var myHtml =  '<div class="row">'+
-                    '<div class="col-lg-2 col-md-2 col-sm-2 avatar"><img src="ars.jpg"/></div>'+
-                    '<div class="col-lg-10 col-md-10 col-sm-10 message"><p>' + msg + '</p></div>'+
+                    '<div class="col-lg-2 col-md-2 col-sm-2 avatar"><img src="ars.jpg"/><span>'+msg.user+'</span></div>'+
+                    '<div class="col-lg-10 col-md-10 col-sm-10 message"><p>' + msg.message + '</p></div>'+
                     '</div>';
         $("#game-content").append(myHtml);
     });
