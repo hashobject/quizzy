0147adc73d2fd80a1278686abd186f81aab8e1ee
Pavel Podviaznikov <pavel.podviaznikov@gmail.com>
2015-02-08 11:52:02 +0200

    online users ready

--- public/main.js
+++ public/main.js
@@ -22,7 +22,12 @@ function submitAnswer(){
     $('#user-answer').keydown(function(e){
         var answerValue = $('#user-answer').val(),
             nickname = $('#nickname').val();
+
         if(e.keyCode === 13){
+            if(name !== nickname){
+                socket.emit('user created', nickname);
+                name = nickname;
+            }
             newMessage(nickname, answerValue);
         }
     });
@@ -64,8 +69,21 @@ function displayLeaders() {
 function onlineUsers(){
     var socket = io();
     socket.on('online users', function(users){
-        console.log(users);
+        $('#online').empty();
+        for(var i in users){
+            $('#online').append('<p>'+users[i].name+'</p>');
+        }
     });
 }
 
+function greetingsFromBot(){
+    var socket = io();
+    socket.on('user greeting', function(greeting){
+        var myHtml =  '<div class="row">'+
+            '<div class="col-lg-2 col-md-2 col-sm-2 avatar"><img src="ars.jpg"/><span>BOT</span></div>'+
+            '<div class="col-lg-10 col-md-10 col-sm-10 message"><p>' + greeting + '</p></div>'+
+            '</div>';
+        $("#game-content").append(myHtml);
+    });
+}
 
