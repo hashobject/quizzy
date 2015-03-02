0c049beb53814ec95cb42f68cbfe16a42e006b9a
PavelPodviaznikov <pavel.podviaznikov@gmail.com>
2015-03-02 12:01:49 +0200

    country to genitive form done

--- questions.js
+++ questions.js
@@ -2,12 +2,12 @@
 var questions = [],
     fs = require('fs'),
 	JSON5 = require('json5'),
-    correctNameEnding,
+    genitiveCountry,
     jsonData,
     consonantals = ["б", "в", "г", "ґ", "д", "ж", "з", "к", "л", "м", "н", "п", "р", "с", "т", "ф", "х", "ц", "ч", "ш", "щ", "й"],
     vowels = ["а", "е", "и", "і", "о", "у", "є", "ю", "я", "ї"];
 
-var originCountries = ['Україна', 'Англія', 'Франція', 'Іспанія', 'Німеччина', 'Чехія', 'Молдова', 'Уельс', 'Китай', 'Азербайджан', 'Чилі'];
+var originCountries = ['Україна', 'Англія', 'Франція', 'Іспанія', "Конго",'Німеччина', 'Чехія', 'Молдова', 'Уельс', 'Китай', 'Азербайджан', 'Чилі', "Беліз", "Бангладеш"];
 
 //fs.readFile('./public/freebase_countries.json', 'utf8', function(err, data){
 //    jsonData = JSON5.parse(data);
@@ -15,50 +15,44 @@ var originCountries = ['Україна', 'Англія', 'Франція', 'Іс
 //    console.log(questionsJS.questions);
 //});
 
-for(var i in originCountries) {
-    correctNameEnding = changeCountryNameEnding(originCountries[i]);
-    questions.push(correctNameEnding);
-}
+originCountries.forEach(function(country){
+    genitiveCountry = countryNameToGenitiveForm(country);
+    questions.push(genitiveCountry);
+});
 
-function changeCountryNameEnding(country) {
-    var countryLength = country.length,
-        endings = country[countryLength-1],
-        nameWithoutEnding = country.slice(0, countryLength-1),
-        correctName;
+function countryNameToGenitiveForm (country) {
 
-    switch(endings) {
-        case 'а':
-            correctName = nameWithoutEnding+'и';
-            break;
-        case 'я':
-            correctName = nameWithoutEnding+'ї';
-            break;
-        case 'й':
-            correctName = nameWithoutEnding+'я';
-            break;
-        case 'н':
-            correctName = nameWithoutEnding+'на';
-            break;
-        case 'р':
-            correctName = nameWithoutEnding+'ра';
-            break;
-        case 'с':
-            correctName = nameWithoutEnding+'са';
-            break;
-        case 'ь':
-            correctName = nameWithoutEnding+'і';
-            break;
-        case 'м':
-            correctName = nameWithoutEnding+'ма';
-            break;
-        case 'и':
-            correctName = nameWithoutEnding;
-            break;
-        default:
-            correctName = country;
+    var countryEnding = country[country.length-1],
+        countryWithoutEnding = country.slice(0, country.length - 1),
+        newCountryForm;
+
+    for(var i in consonantals){
+
+        if(countryEnding === consonantals[i] && countryEnding !== 'й' && countryEnding !== 'ш' && countryEnding !== 'з'){
+            newCountryForm = country + 'у';
+        }
+        else if(countryEnding === 'й'){
+            newCountryForm = countryWithoutEnding + "ю";
+        }
+        else if(countryEnding === 'ш'){
+            newCountryForm = country;
+        }
+        else if(countryEnding === 'з'){
+            newCountryForm = country + 'а';
+        }
     }
 
-    return correctName;
+    if(countryEnding === 'я'){
+        newCountryForm = countryWithoutEnding + 'ї';
+    }
+    else if(countryEnding === 'а'){
+        newCountryForm = countryWithoutEnding + 'и';
+    }
+    else if(countryEnding === 'і' || countryEnding === 'о' || countryEnding === 'у' || countryEnding === 'е'){
+        newCountryForm = country;
+    }
+
+    return newCountryForm;
 }
 
 //function getDataFromJson(json, question) {
