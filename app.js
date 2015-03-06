43daec51b5b0bfcc83a9e0f52d92543708c394dd
PavelPodviaznikov <pavel.podviaznikov@gmail.com>
2015-03-06 15:30:45 +0200

    real-time leaders, every correct answer +1 point

--- app.js
+++ app.js
@@ -9,20 +9,18 @@ var question,
     questionsList = JSON.parse(fs.readFileSync('./generatedCountries.json', 'utf8'));
 
 var users = [];
-var leaders = [{name: 'Anton', points: 50}, {name: 'Pasha', points: 60}, {name: 'Maryna', points: 70}];
-
 
 question = getRandomQuestion();
-
-sendLeaders(leaders);
 
 sendQuestion(question)
 
 io.on('connection', function(socket){
 
+    io.emit('leaders', users);
+
   socket.on('user created', function(user){
     io.emit('user greeting', 'Hello, ' + user + '!');
-    users.push({name: user, connectionId: socket.id});
+    users.push({name: user, connectionId: socket.id, points: 0});
     io.emit('online users', users);
   });
 
@@ -31,7 +29,8 @@ io.on('connection', function(socket){
   socket.on('message', function(msg){
     var answer = msg.message;
     io.emit('message', msg);
-    io.emit('answer', checkTheAnswer(answer));
+    io.emit('answer', checkTheAnswer(answer, socket));
+      io.emit('leaders', users);
   });
 
   socket.on('disconnect', function(){
@@ -52,19 +51,6 @@ http.listen(app.get('port'), function(){
   console.log('listening on *:3000');
 });
 
-function sendLeaders(leaders){
-    app.get('/leaders', function(request, response) {
-        leaders.map(function(leader){
-            leader.points = Math.floor(Math.random() * (100 - 0 + 1)) + 0;
-        });
-
-        leaders.sort(function(obj1, obj2){
-            return obj2.points - obj1.points;
-        });
-
-        response.send(leaders);
-    });
-}
 
 function sendQuestion(question){
     app.get('/question', function(request, response) {
@@ -77,8 +63,13 @@ function getRandomQuestion(){
     return JSON.parse(randomQuestion);
 }
 
-function checkTheAnswer(userAnswer){
+function checkTheAnswer(userAnswer, socket){
     if(question.answer.toLowerCase() === userAnswer.toLowerCase()){
+        users.map(function(user){
+            if(user.connectionId === socket.id){
+                user.points = user.points + 1;
+            }
+        });
         question = getRandomQuestion();
         return question;
     }
